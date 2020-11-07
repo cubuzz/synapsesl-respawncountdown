164f0a28275bd770e6a102268657203dddd6d4f7
GrafDimenzio <crafterwolfhd@gmail.com>
2020-11-07 17:38:03 +0100

    Update

--- Example Plugin/EventHandlers.cs
+++ Example Plugin/EventHandlers.cs
@@ -8,6 +8,15 @@ namespace Example_Plugin
         {
             //You can also use SynapseController.Server to get the Server!
             Server.Get.Events.Player.PlayerJoinEvent += OnJoin;
+            Server.Get.Events.Round.RoundStartEvent += OnStart;
+        }
+
+        private void OnStart()
+        {
+            foreach(var player in Server.Get.Players)
+            {
+                player.GiveTextHint("The Round started!");
+            }
         }
 
         private void OnJoin(Synapse.Api.Events.SynapseEventArguments.PlayerJoinEventArgs ev)
